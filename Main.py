c1693356ecc82e78c07c7aa7a4ae0d5bac7b9e08
BeckSM64 <becksm64@gmail.com>
2023-01-11 14:38:58 -0500

    :sparkles: add argparse to take data location as input from user

--- Main.py
+++ Main.py
@@ -1,13 +1,33 @@
 #!/usr/bin/env python3
 
+import argparse
+import sys
+
 from JsonParser import JsonParser
 from HtmlGenerator import HtmlGenerator
 
 def main():
+
+    # Parse arguments
+    argparser = argparse.ArgumentParser()
+    argparser.add_argument("data_path", help="chat_history.json exported from Snapchat")
+    args = argparser.parse_args()
+
+    # Check args passed in for validity
+    path_to_chat_history = args.data_path
+    path_to_chat_history_list = path_to_chat_history.split('/')
+
+    # get last element in list, should be file name
+    chat_history_file_name = path_to_chat_history_list[-1]
+
+    # check if file name is "chat_history.json"
+    if chat_history_file_name != "chat_history.json":
+        print("[ERROR] Incorrect file, should be \"chat_history.json\"")
+        sys.exit(-1)
     
     # Parse chat history JSON
     jsonParser = JsonParser()
-    chat_history_json_list = jsonParser.get_json_string_from_file("./SnapchatData/mydata/json/chat_history.json")
+    chat_history_json_list = jsonParser.get_json_string_from_file(path_to_chat_history)
 
     # Generate sorted chat history html file
     htmlGenerator = HtmlGenerator()
